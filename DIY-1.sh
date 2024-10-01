65cc0efe4e437018281904e889b5ca834a22e63f
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-10-01 08:57:29 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -5,6 +5,11 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 git clone https://github.com/WukongMaster/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
 
 # 3-系统主题
-# git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
 git clone https://github.com/WukongMaster/luci-theme-teleofis.git package/luci-theme-teleofis
 
+# 4-添加 OpenClash 插件
+sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+
+# 5-添加 PassWall 插件
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
